3e383eca3df479d965790cea8b3cfb08cc4d5c28
swoo <kokodd1234@gmail.com>
2026-02-25 11:43:22 +0900

    [Feat] 예외 처리에 코딩컨벤션 적용 예외 던지는 코드 BaseException.from(상태열거값)
    예외 처리하는 코드
    GlobalExceptionHandler에서 전부 처리

--- src/main/java/com/example/demo/user/UserService.java
+++ src/main/java/com/example/demo/user/UserService.java
@@ -1,5 +1,6 @@
 package com.example.demo.user;
 
+import com.example.demo.common.exception.BaseException;
 import com.example.demo.user.model.AuthUserDetails;
 import com.example.demo.user.model.EmailVerify;
 import com.example.demo.user.model.User;
@@ -13,6 +14,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+import static com.example.demo.common.model.BaseResponseStatus.*;
+
 @RequiredArgsConstructor
 @Service
 public class UserService implements UserDetailsService {
@@ -22,6 +25,12 @@ public class UserService implements UserDetailsService {
     private final EmailVerifyRepository emailVerifyRepository;
 
     public UserDto.SignupRes signup(UserDto.SignupReq dto) {
+
+        // 이메일 중복 확인
+        if(userRepository.findByEmail(dto.getEmail()).isPresent()) {
+            throw BaseException.from(SIGNUP_DUPLICATE_EMAIL);
+        }
+
         User user = dto.toEntity();
         user.setPassword(passwordEncoder.encode(dto.getPassword()));
         userRepository.save(user);
@@ -39,13 +48,13 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(username).orElseThrow();
+        User user = userRepository.findByEmail(username).orElseThrow(() -> BaseException.from(LOGIN_INVALID_USERINFO));
 
         return AuthUserDetails.from(user);
     }
 
     public void verify(String uuid) {
-        EmailVerify emailVerify = emailVerifyRepository.findByUuid(uuid).orElseThrow();
+        EmailVerify emailVerify = emailVerifyRepository.findByUuid(uuid).orElseThrow(() -> BaseException.from(SIGNUP_INVALID_UUID));
         User user = userRepository.findByEmail(emailVerify.getEmail()).orElseThrow();
         user.setEnable(true);
         userRepository.save(user);
